0d522a2e2c007bfb9c3a733e394aab5e872f3d61
Manisa Bachtiar <manisabachtiar17@gmail.com>
2024-01-13 16:15:45 +0700

    Membuat Responsive navbar

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,30 @@
 module.exports = {
   content: ["./public/**/*.{html,js}"],
   theme: {
+    screens: {
+      '2xs': '360px',
+      // => @media (min-width: 360px) { ... }
+
+      'xs': '475px',
+      // => @media (min-width: 475px) { ... }
+
+      'sm': '520px',
+      // => @media (min-width: 520px) { ... }
+
+      '2sm': '640px',
+      // => @media (min-width: 640px) { ... }
+
+      'md': '768px',
+      // => @media (min-width: 768px) { ... }
+
+      'lg': '1024px',
+      // => @media (min-width: 1024px) { ... }
+
+      'xl': '1280px',
+      // => @media (min-width: 1280px) { ... }
+      '2xl': '1536px',
+      // => @media (min-width: 1536px) { ... }
+    },
     extend: {
       fontFamily: {
         custom: ["Rubik"],
