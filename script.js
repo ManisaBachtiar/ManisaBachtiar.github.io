3059b56e736146a3d4affca0ad6a3eb9431209dc
Manisa Bachtiar <manisabachtiar17@gmail.com>
2024-01-19 15:20:35 +0700

    This might be final commit

--- script.js
+++ script.js
@@ -25,22 +25,12 @@ let close=document.getElementsByClassName('close')[0];
 let navbar= document.getElementsByClassName('navbar')[0];
 
 function toggle() {
-    if (navbar.classList.contains('hidden') || close.classList.contains('hidden')) {
-        navbar.classList.remove('hidden');
-        close.classList.remove('hidden');
-    } else {
-        navbar.classList.add('hidden');
-        close.classList.add('hidden');
-    }
+    navbar.classList.toggle('hidden')
+    close.classList.toggle('hidden')
 }
 function toggle2() {
-    if (navbar.classList.contains('hidden') || close.classList.contains('hidden')) {
-        navbar.classList.remove('hidden');
-        close.classList.remove('hidden');
-    } else {
-        navbar.classList.add('hidden');
-        close.classList.add('hidden');
-    }
+    navbar.classList.toggle('hidden')
+    close.classList.toggle('hidden')
 }
 close.addEventListener('click', toggle);
 bar.addEventListener('click', toggle);
