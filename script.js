f33d52a8c6fdcf7cb0e0567fca3a5016b61a784e
Manisa Bachtiar <manisabachtiar17@gmail.com>
2024-01-19 08:51:26 +0700

    Membuat backend form

--- script.js
+++ script.js
@@ -1,3 +1,25 @@
+const scriptURL = 'https://script.google.com/macros/s/AKfycbw4w5CNGwkej9UxeaZlY9v4AbO6w5YktbaHT9h_nleVogUXtj8Yg4f5wu6azPjm0vi_tQ/exec'
+const form = document.forms['submit-to-google-sheet']
+const btnSend =document.querySelector('.submit');
+const load = document.querySelector('.loading');
+form.addEventListener('submit', e => {
+  e.preventDefault()
+  load.classList.toggle('hidden');
+  btnSend.classList.toggle('hidden');
+  fetch(scriptURL, { method: 'POST', body: new FormData(form)})
+    .then(response => {
+        load.classList.toggle('hidden');
+        btnSend.classList.toggle('hidden');
+    
+        Swal.fire({
+        title: 'Success!',
+        text: 'Your message has been successfully sent.',
+        icon: 'Success',
+        confirmButtonText: 'Ok'
+      });
+    })
+    .catch(error => console.error('Error!', error.message))
+})
 let bar= document.getElementsByClassName('fa-bars')[0];
 let close=document.getElementsByClassName('close')[0];
 let navbar= document.getElementsByClassName('navbar')[0];
@@ -22,3 +44,4 @@ function toggle2() {
 }
 close.addEventListener('click', toggle);
 bar.addEventListener('click', toggle);
+
