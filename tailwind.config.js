40d2a19f9701afb45e304c134026dda521361bc0
Manisa Bachtiar <manisabachtiar17@gmail.com>
2024-02-11 19:17:25 +0700

    im back

--- tailwind.config.js
+++ tailwind.config.js
@@ -26,6 +26,8 @@ module.exports = {
 
       'xl': '1280px',
       // => @media (min-width: 1280px) { ... }
+      '1/2xl': '1410px',
+      // => @media (min-width: 1280px) { ... }
       '2xl': '1536px',
       // => @media (min-width: 1536px) { ... }
     },
