87e1a25e21749b00e35e1ba013b6c104f07cb12c
Manisa Bachtiar <manisabachtiar17@gmail.com>
2024-02-19 21:34:15 +0700

    Add some link in buttons

--- script.js
+++ script.js
@@ -14,7 +14,6 @@ form.addEventListener('submit', e => {
     .then(response => {
         load.classList.toggle('hidden');
         btnSend.classList.toggle('hidden');
-    
         Swal.fire({
         title: 'Success!',
         text: 'Your message has been successfully sent.',
@@ -41,26 +40,6 @@ bar.addEventListener('click', toggle);
 document.getElementById('btn').addEventListener('click', function() {
   window.location.href = '#about';
 });
-
-
-
-const buttonUrls = [
-  'https://github.com/ManisaBachtiar/Food-Choices',
-  'https://food-choices.vercel.app/',
-  'https://github.com/ManisaBachtiar/manisaBachtiar.github.io',
-  'https://manisabachtiar.github.io/',
-  'https://dribbble.com/shots/17144625-Cats-Marketplace',
-  'https://dribbble.com/shots/17219512-English-Course'
-];
-
-for (let i = 1; i <= buttonUrls.length; i++) {
-  const btn = document.getElementById(`btn${i}`);
-  if (btn) {
-    btn.addEventListener('click', function() {
-      window.location.href = buttonUrls[i - 1];
-    });
-  }
-}
 buttons.addEventListener("click", function (e) {
   const id = e.target.dataset.id;
   
@@ -88,7 +67,9 @@ const cardItems = [
     desc2: "In this project, I've gained knowledge about working with APIs, fetching and filtering data.I've also learned how to implement pagination, all using JavaScript, without relying on any frameworks.This experience has significantly enhanced my JavaScript skills.",
     btn1:"HTML",
     btn2:"CSS",
-    btn3:"Javascript"
+    btn3:"Javascript",
+    link:"https://food-choices.vercel.app/",
+    link2:"https://github.com/ManisaBachtiar/Food-Choices"
   },
   {
     img1:"img/portofolio1.png" ,
@@ -99,39 +80,41 @@ const cardItems = [
     desc2: "Through this project, I honed my design skills using Tailwind CSS and JavaScript. I customized the visuals to match my preferred theme of black and pink, leading to improved clarity in expressing my ideas.",
     btn1:"HTML",
     btn2:"Tailwind",
-    btn3:"Javascript"
+    btn3:"Javascript",
+    link:"https://manisabachtiar.github.io/",
+    link2:"https://github.com/ManisaBachtiar/manisaBachtiar.github.io "
   },
-  {
-    img1:"img/course1.png" ,
-    img2:"img/course2.png",
-    title1: "English Course",
-    miniTitle: "UI/UX Design",
-    desc1:"I've crafted a UI/UX design for an English course that seamlessly blends course materials with mentor support for a flexible online learning experience.",
-    desc2: "So, in this project, I got the lowdown on whipping up reviews and threw together a slick logo for the course icon. It was a hands-on dive into making things look good and getting the word out in style.",
-    btn1:"Figma",
-    btn2:"Logo",
-    btn3:"Layouting"
-  },
-  {
-    img1:"img/marketPlace1.png" ,
-    img2:"img/marketPlace2.png",
-    title1: "Cat MarketPlace",
-    miniTitle: "UI/UX Design",
-    desc1:"I’m cooking up a UI/UX project to make life awesome for cat lovers. It's all about finding cats, snagging their favorite goodies and accessories, and throwing in some tips for a healthy kitty lifestyle. We're crafting a platform that's so user-friendly, it'll have cat enthusiasts feeling like pet care rockstars! ",
-    desc2: "In my first go at UI/UX, I got the hang of using Figma. I played around with designs, making sure they not only looked good but also had that extra charm to make them super appealing.",
-    btn1:"HTML",
-    btn2:"CSS",
-    btn3:"Javascript"
-  }
+  {img1:"img/course1.png" ,
+  img2:"img/course2.png",
+  title1: "English Course",
+  miniTitle: "UI/UX Design",
+  desc1:"I've meticulously put together a user-friendly interface and learning experience, seamlessly integrating course content with mentor guidance for an adaptable online learning adventure that maximizes student engagement and comprehension.",
+  desc2: "So, during this project, I delved into crafting impressive reviews and assembling a polished logo for the course icon. It was a hands-on journey, focused on enhancing aesthetics and effectively sharing information with a touch of sophistication.",
+  btn1:"Figma",
+  btn2:"Logo",
+  btn3:"UI/UX",
+  link:"https://dribbble.com/shots/17219512-English-Course",
+},
+{
+  img1:"img/marketPlace1.png" ,
+  img2:"img/marketPlace2.png",
+  title1: "Cat MarketPlace",
+  miniTitle: "UI/UX Design",
+  desc1:"I’m cooking up a UI/UX project to make life awesome for cat lovers. It's all about finding cats, snagging their favorite goodies and accessories, and throwing in some tips for a healthy kitty lifestyle. We're crafting a platform that's so user-friendly, it'll have cat enthusiasts feeling like pet care rockstars! ",
+  desc2: "In my first go at UI/UX, I got the hang of using Figma. I played around with designs, making sure they not only looked good but also had that extra charm to make them super appealing.",
+  btn1:"Design",
+  btn2:"Figma",
+  btn3:"UI/UX",
+  link:"https://dribbble.com/shots/17144625-Cats-Marketplace",
+}
 ]
 window.addEventListener("DOMContentLoaded", function () {
-  displayCard(cardItems);
+  displayCards(cardItems); // Memanggil fungsi untuk menampilkan semua kartu
 });
-let container = document.querySelector(".containerCard");
-function displayCard(cardItemsItems) {
+
+function displayCards(cardItems) {
   let display = cardItems.map(function (item) {
-    return ` 
-    
+    return `
     <div class="mx-auto grid pb-9 rounded-md 2md:grid-cols-2 bg-zinc-900 border border-zinc-700 w-[90%] sm:px-9 px-0 2md:px-7 py-5 my-5">
       <div class="w-[100%] ">
         <img class="rounded-lg mx-auto 2md:hidden w-[85%]  sm:w-[90%]" src="${item.img2}" alt="">
@@ -140,7 +123,7 @@ function displayCard(cardItemsItems) {
           <div class="flex flex-row text-white sm:ml-[-16px] font-custom  ">
             <button class="bg-[#FE1A7E] bg-opacity-65 px-2 py-1 rounded-lg sm:px-4">${item.btn1}</button>
             <button class="ml-5 bg-[#FE1A7E] px-2 sm:px-4 bg-opacity-65 py-1 rounded-lg">${item.btn2}</button>
-            <button class="ml-5 bg-[#FE1A7E] px-2 sm:px-4 bg-opacity-65 py-1 sm-px-4 rounded-lg">${item.btn3}</button>
+            ${item.btn3 ? `<button class="ml-5 bg-[#FE1A7E] px-2 sm:px-4 bg-opacity-65 py-1 sm-px-4 rounded-lg">${item.btn3}</button>` : ''}
           </div>
         </div>
       </div>
@@ -153,14 +136,15 @@ function displayCard(cardItemsItems) {
           <p class="">${item.desc2}</p>
         </div>
         <div class="lg:ml-[-80px] 2md:ml-[-10px] xl:ml-[-105px] text-white 2md:px-9 md:px-5 px-7">
-          <button class="border border-pink-200 px-5 py-2 font-custom rounded-lg">See Demo</button>
-          <button class="border border-pink-200 px-5 py-2 ml-2 font-custom rounded-lg">See Source</button>
+          <button class=" hover:bg-transparent hover:border hover:border-white bg-pink-700 px-5 py-2 font-custom rounded-lg"><a href="${item.link}">See Demo</a></button>
+          ${item.link2 ? `<button class="border hover:bg-pink-700 hover:border-0 border-pink-200 px-5 py-2 ml-2 font-custom rounded-lg"><a href="${item.link2}">See Source</a></button>` : ''}
         </div>
       </div>
     </div>
-  
-    `
+    `;
   });
+
+  let container = document.querySelector(".containerCard");
   display = display.join("");
   container.innerHTML = display;
 }
